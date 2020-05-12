ac7e2f3e3c58c0d028fbb5efa505564d8751667c
'booldook' <'gjsxo103@naver.com'>
2020-05-12 16:15:59 +0900

    "2)수정"

--- src/com/javaex/practice04/Ex06.java
+++ src/com/javaex/practice04/Ex06.java
@@ -5,16 +5,19 @@ public class Ex06 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		char c[] = { 'T', 'H', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 'p', 'e', 'n', 'c', 'i', 'l' };
+		char copy[] = new char[16]; // 복사본
 
-		for (int i = 0; i < c.length; i++) { //배열 프린트
+		for (int i = 0; i < c.length; i++) { // 배열 프린트
 			System.out.print(c[i]);
+			copy[i] = c[i]; // c[i]값을 copy[i]값으로 복사
 		}
-		System.out.println();		//지정 배열값을 덮어씌우 변경
-		c[4] = ',';
-		c[7] = ',';
-		c[9] = ',';
-		for (int i = 0; i < c.length; i++) {	//바뀐 배열값을 출력
-			System.out.print(c[i]);
+
+		System.out.println(); // 지정 배열값을 덮어씌우 변경
+		copy[4] = ',';
+		copy[7] = ',';
+		copy[9] = ',';
+		for (int i = 0; i < c.length; i++) { // 바뀐 배열값을 출력
+			System.out.print(copy[i]);
 		}
 	}
 
